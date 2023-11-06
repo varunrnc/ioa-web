cda2904d5d357902f66f60503762513e5442e2d1
Mramkk <rramkumarbedia@gmail.com>
2023-11-06 13:01:06 +0530

    reset all

--- app/Http/Controllers/Admin/Slider/AdminSliderController.php
+++ app/Http/Controllers/Admin/Slider/AdminSliderController.php
@@ -32,11 +32,7 @@ class AdminSliderController extends Controller
     }
     public function save(Request $req)
     {
-        if ($req->hasFile('image')) {
-            return ApiRes::success("has file");
-        } else {
-            return ApiRes::success("no  file");
-        }
+
 
         $req->validate([
             'slider_name' => 'required|string|max:225',
